d5db46439430ee67ec5719e454e76d38d0f2e371
Dorion Mitchell <dorionmitchell@Dorions-MacBook-Pro.local>
2024-01-27 22:28:16 -0500

    base madlibs app

---
+++ madlibs/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+import MadLibsForm from './components/MadLibsForm';
+import MadLibsText from './components/MadLibsText';
+import MadLibsImages from './components/MadLibsImages';
+
+function App() {
+  const [word, setWords] = useState("");
+  
+  const handleFormSubmit = (values) => {
+    const newWords = {
+      noun: values.noun,
+      noun2: values.noun2,
+      adjective: values.adjective,
+      color: values.color
+    }
+    setWords(newWords);
+  };
+
+  return (
+    <div>
+      <MadLibsImages/>
+      <div id="madlibsAppContainer">
+        <h1>Mad Libs!</h1>
+        {!word ?
+        <MadLibsForm onFormSubmit={handleFormSubmit}/>
+        :
+        <MadLibsText noun={word.noun} noun2={word.noun2} adjective={word.adjective} color={word.color}/>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ madlibs/src/components/MadLibsImages.js
@@ -0,0 +1,7 @@
+const MadLibsImages = () => {
+    return (
+        <img id="madLibsImage" src="https://img.freepik.com/premium-photo/mad-libs-2d-cartoon-illustraton-white-background-high_889056-26484.jpg" style={{opacity:0.2, width:'100%', height:'auto'}}></img>
+    )
+}
+
+export default MadLibsImages;
