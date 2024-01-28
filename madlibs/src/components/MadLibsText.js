5c34fe5a5b14a6a6cf22fd04e8f263ee3db8989a
Dorion Mitchell <dorionmitchell@Dorions-MacBook-Pro.local>
2024-01-27 23:28:24 -0500

    added reset button. added styling

--- madlibs/src/components/MadLibsText.js
+++ madlibs/src/components/MadLibsText.js
@@ -1,7 +1,12 @@
-function MadLibsText({ noun, noun2, adjective, color}) {  
+import './Component.css';
+
+function MadLibsText({ noun, noun2, adjective, color, onBtnClick}) {  
     const newStory = `There was a ${noun} ${noun2} who loved a ${adjective} ${color}`;
      return (
+      <div>
         <div>{newStory}</div>
+        <button onClick={onBtnClick} id="restart">Restart</button>
+      </div>
     );
   };
   
